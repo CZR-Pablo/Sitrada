51b4ac8bb2896044915f116373951f717583d476
CZR-Pablo <pablo.ciezar@inrap.fr>
2017-04-24 17:19:58 +0200

    Add files via upload

---
+++ PackagesAInstaller.R
@@ -0,0 +1,12 @@
+
+##############
+# Conserver la liste des packages installes pour les MaJ R
+install.packages("ade4")
+install.packages("adegraphics")
+install.packages("tidyverse")
+install.packages("stringdist")
+install.packages("ggmap")
+install.packages("Unicode")
+install.packages("FactoMineR")
+install.packages("leaflet")
+install.packages("ggdendro")
